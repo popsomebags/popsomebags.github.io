390f07270515e3a167b3789bdd64da82156cc67f
will <aconfidentmeow@gmail.com>
2022-10-24 14:49:03 -0400

    fixing script

--- itis3135/scripts/default.js
+++ itis3135/scripts/default.js
@@ -1,9 +1,9 @@
-let today = new Date();
-const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
 function displayDate(){
-    document.getElementById("display_date").innerHTML = "Today is " + today.getHours() + ":" + today.getMinutes() + " on " + weekdays[today.getDay()] +
+    let today = new Date();
+    const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    document.getElementById("date").innerHTML = "Today is " + today.getHours() + ":" + today.getMinutes() + " on " + weekdays[today.getDay()] +
         ", " + today.getDate() + " " + month[today.getMonth()] + ", " + today.getFullYear();
 }
 
