59fe75573242c6bc66caca4a3e557ee1f5542e8d
will <aconfidentmeow@gmail.com>
2022-10-24 17:28:39 -0400

    updated first scripts and css

--- itis3135/scripts/default.js
+++ itis3135/scripts/default.js
@@ -12,4 +12,34 @@ function displayDate(){
 function welcomeUser(){
     document.getElementById("response").innerHTML = "Wonderful Zebra Ltd. welcomes you, " + document.getElementById("name").value + 
         "! We're glad you are doing " + document.getElementById("feeling").value + "!";
+}   
+
+function generateColor(){
+    var colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"];
+    var color = colors[Math.floor(Math.random()*colors.length)];
+    document.getElementById("color").innerHTML = "Your color is: " + color;
+}
+
+function generateMood(){
+    var moods = ["Happy", "Sad", "Anxious", "Romantic", "Angry", "Bored", "Tired"];
+    var mood = moods[Math.floor(Math.random()*moods.length)];
+    document.getElementById("mood").innerHTML = "Your mood is: " + mood;
+}
+
+function generateShape(){
+    var shapes = ["Triangle", "Rectangle", "Square", "Circle", "Pentagon", "Hexagon", "Trapazoid"];
+    var shape = shapes[Math.floor(Math.random()*shapes.length)];
+    document.getElementById("shape").innerHTML = "Your shape is: " + shape;
+}
+
+function generateAction(){
+    var actions = ["Jump", "Punch", "Kick", "Toss", "Shout", "Move", "Swing"];
+    var action = actions[Math.floor(Math.random()*actions.length)];
+    document.getElementById("action").innerHTML = "Your action is: " + action;
+}
+
+function generateFood(){
+    var foods = ["Steak", "Mac And Cheese", "Spaghetti", "French Fries", "Sushi", "Ramen", "Burrito"];
+    var food = foods[Math.floor(Math.random()*foods.length)];
+    document.getElementById("food").innerHTML = "Your food is: " + food;
 }
