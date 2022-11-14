7c291743f5f4d5ca91ef5d135958ad7716cb285f
will <aconfidentmeow@gmail.com>
2022-11-14 10:33:51 -0500

    updated all pages

---
+++ itis3135/scripts/arrays.js
@@ -0,0 +1,46 @@
+var employee = [];
+var salary = [];
+
+function addSalary()
+{
+    var employeeName = document.getElementById('employeeName').value;
+    var employeeSalary = parseInt(document.getElementById('employeeSalary').value);
+
+    if ((employeeSalary == "") || (isNaN(employeeSalary))) {
+        alert("Please enter a valid salary!");
+        inputSalary = "";
+        document.getElementById("employee").focus();
+    }else{
+        if(employee.includes(employeeName)){
+            var index = employee.indexOf(employeeName);
+            salary[index] = employeeSalary;
+        }else{
+            employeeSalary = parseFloat(employeeSalary);
+            employee.push(employeeName);
+            salary.push(employeeSalary);
+            document.getElementById("salary").value = "";
+            document.getElementById("employee").focus();   
+        }
+    }
+}
+
+function displayResults() {
+    var high = 0;
+    const average = salary.reduce((a, b) => a + b) / salary.length;
+    for(j = 0; j < salary.length; j++){
+        if(salary[j] > high){
+            high = salary[j];
+        }
+    }
+    document.getElementById("results").innerHTML = "<h2>Results</h2>" + "<p>The Average Salary is: $" + average + "</p>" + "<p>, and the highest salary is: $" + high + "</p>";
+}
+
+function displaySalary() {
+    var table;
+    table = "<table><tr><th>Employee</th><th>Salary</th></tr>"
+    for (i = 0; i < salary.length; i++){
+        table += "<tr><td>" + employee[i] + "</td><td>" + salary[i] + "</td></tr>";
+    }
+    table += "</table>";
+    document.getElementById("results_table").innerHTML = table;
+}
